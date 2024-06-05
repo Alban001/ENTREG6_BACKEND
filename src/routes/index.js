37aee89db4206d6635f9ccd82f92dfdafe9c303d
Elias Alban <Alban001@users.noreply.github.com>
2024-06-04 20:10:01 -0400

    router arreglado

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,20 @@
 const express = require('express');
-const userRouter = require('./user.router');
-const reviewRouter = require('./review.router');
-const imageRouter = require('./image.router');
-const hotelRouter = require('./hotel.router');
-const cityRouter = require('./city.router');
-const bookingRouter = require('./booking.router');
+const userRouter = require('../routes/user.router');
+const reviewRouter = require('../routes/review.router');
+const imageRouter = require('../routes/image.router');
+const hotelRouter = require('../routes/hotel.router');
+const cityRouter = require('../routes/city.router');
+const bookingRouter = require('../routes/booking.router');
 
 const router = express.Router();
 
 // colocar las rutas aquí
-router.use('/users', userRouter);
-router.use('/reviews', reviewRouter);
-router.use('/images', imageRouter);
-router.use('/hotels', hotelRouter);
-router.use('/cities', cityRouter);
-router.use('/bookings', bookingRouter);
+router.use(userRouter);
+router.use(reviewRouter);
+router.use(imageRouter);
+router.use(hotelRouter);
+router.use(cityRouter);
+router.use(bookingRouter);
 
 
 module.exports = router;
